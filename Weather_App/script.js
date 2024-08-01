6ff45c7d63abe0b1cf6f2511ed890aec3030c7ed
Jacob Cortez <jacob.cortez@teamseco.com>
2024-08-01 11:30:27 -0400

    Weather App Created

--- Weather_App/script.js
+++ Weather_App/script.js
@@ -1,5 +1,10 @@
 'use strict';
 
+let forecastPeriods = [];
+const header = document.querySelector('.test');
+const body = document.querySelector('.test2');
+const section = document.querySelector('.head');
+
 async function getWeather() {
   const weatherCall = await fetch(
     'https://api.weather.gov/gridpoints/FFC/70,82/forecast',
@@ -11,10 +16,25 @@ async function getWeather() {
   return await weatherCall.json();
 }
 getWeather().then((result) => {
-  const forecastName = result.properties.periods[0].name;
-  const forecast = result.properties.periods[0].detailedForecast;
-  console.log(forecastName);
-
-  document.querySelector('.test').textContent = forecastName;
-  document.querySelector('.test2').textContent = forecast;
+  forecastPeriods = [...result.properties.periods];
+  updateData(forecastPeriods);
 });
+
+function updateData(forecast) {
+  forecast.forEach((period) => {
+    const forecastImg = document.createElement('img');
+    const forecastDiv = document.createElement('div');
+    forecastDiv.classList.add('forecast__div');
+    const forecastPeriod = document.createElement('h1');
+    forecastPeriod.classList.add('forecast__day');
+    const forecastDetails = document.createElement('p');
+    forecastDetails.classList.add('forecast__details');
+    forecastPeriod.innerHTML = period.name;
+    forecastDetails.innerHTML = period.detailedForecast;
+    forecastImg.src = period.icon;
+    section.append(forecastDiv);
+    forecastDiv.append(forecastPeriod);
+    forecastDiv.append(forecastDetails);
+    forecastDiv.append(forecastImg);
+  });
+}
